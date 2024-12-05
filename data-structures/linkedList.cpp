c31bb6023a5f664b01054e53c254b9572995f3af
Ankit  Adhikari <ankitadhikari@Ankits-MacBook-Air.local>
2024-12-05 12:55:17 +0530

    added delete function in linked list

--- data-structures/linkedList.cpp
+++ data-structures/linkedList.cpp
@@ -34,6 +34,45 @@ public:
         currentNode->next = newNode;
     }
 
+    void deleteNode(int data)
+    {
+        if (this->head == NULL)
+        {
+            cout << "List is empty. Cannot delete." << endl;
+            return;
+        }
+
+        // If the head node is to be deleted
+        if (this->head->data == data)
+        {
+            Node *temp = this->head;
+            this->head = this->head->next;
+            delete temp;
+            return;
+        }
+
+        Node *currentNode = this->head;
+        Node *prevNode = NULL;
+
+        // Traverse the list to find the node to delete
+        while (currentNode != NULL && currentNode->data != data)
+        {
+            prevNode = currentNode;
+            currentNode = currentNode->next;
+        }
+
+        // If the node is not found
+        if (currentNode == NULL)
+        {
+            cout << "Node with value " << data << " not found." << endl;
+            return;
+        }
+
+        // Remove the node
+        prevNode->next = currentNode->next;
+        delete currentNode;
+    }
+
     void printList() {
         Node* currentNode = this->head;
         while (currentNode != NULL) {
@@ -51,6 +90,8 @@ int main() {
     list.addNode(3);
     list.addNode(4);
     list.addNode(5);
+    list.printList();
+    list.deleteNode(3);
     list.printList();
     return 0;
 }
